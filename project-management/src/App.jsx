c2094beeadf087a7912a8f2c662110554cbe055c
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-03-28 11:01:22 +0200

    minor fixes

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -115,6 +115,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8 ">
       <ProjectsSidebar
+        selectedProjectId={projectsState.selectedProjectId}
         onSelectProject={handleSelectProject}
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
