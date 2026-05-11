44a9286413fc542ce55f4da24e3a6537e0c7cab6
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-05-11 13:48:03 +0300

    login form structure

---
+++ form-validation/src/components/Login.jsx
@@ -0,0 +1,66 @@
+import Input from "./Input.jsx";
+import { hasMinLength, isEmail, isNotEmpty } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
+
+export default function Login() {
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailIsInvalid,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordIsInvalid,
+  } = useInput("", (value) => hasMinLength(value, 6));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (emailIsInvalid || passwordIsInvalid) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailIsInvalid && "Please enter a valid email."}
+          required
+        />
+
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordIsInvalid && "Please enter a valid password."}
+          required
+        />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
