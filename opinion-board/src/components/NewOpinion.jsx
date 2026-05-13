95aa8c7c57acbed62435ddf7df571914a691b993
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-05-13 13:01:09 +0300

    disable button on pending submit

--- opinion-board/src/components/NewOpinion.jsx
+++ opinion-board/src/components/NewOpinion.jsx
@@ -1,5 +1,6 @@
 import { use, useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 
 export function NewOpinion() {
   const [formState, formAction] = useActionState(submitOpinion, {
@@ -87,9 +88,7 @@ export function NewOpinion() {
           </ul>
         )}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
